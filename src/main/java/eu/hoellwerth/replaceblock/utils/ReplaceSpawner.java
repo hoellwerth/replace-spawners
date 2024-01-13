09b15a8aa4811658e191bc61fde7c9758f395958
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2024-01-13 23:11:43 +0100

    Fixed Spawner replacement bug with MetaSpawners

--- src/main/java/eu/hoellwerth/replaceblock/utils/ReplaceSpawner.java
+++ src/main/java/eu/hoellwerth/replaceblock/utils/ReplaceSpawner.java
@@ -1,10 +1,12 @@
 package eu.hoellwerth.replaceblock.utils;
 
 import eu.hoellwerth.replaceblock.ReplaceBlock;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 import java.util.Objects;
@@ -20,10 +22,16 @@ public class ReplaceSpawner {
         block.setType(Objects.requireNonNull(Material.getMaterial(Objects.requireNonNull(
                 ReplaceBlock.INSTANCE.getConfig().getString("interimBlock")))));
         block.getState().update();
-        block.setType(Objects.requireNonNull(Material.getMaterial(Objects.requireNonNull(
-                ReplaceBlock.INSTANCE.getConfig().getString("finalBlock")
-        ))));
-        block.getState().update();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                block.setType(Objects.requireNonNull(Material.getMaterial(Objects.requireNonNull(
+                        ReplaceBlock.INSTANCE.getConfig().getString("finalBlock")
+                ))));
+                block.getState().update();
+            }
+        }.runTaskLater(ReplaceBlock.INSTANCE, 20);
+
         block.setMetadata("replacedSpawner", new org.bukkit.metadata.FixedMetadataValue(ReplaceBlock.INSTANCE, true));
         ReplaceBlock.INSTANCE.getLogManager().writeToLog(
                 "Spawner", "Replaced a spawner at " +
